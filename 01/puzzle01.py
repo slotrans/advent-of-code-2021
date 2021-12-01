6b9902ed13862bdf9b2545e2d3b18ab3ce69d0ca
Noah Yetter <nyetter@gmail.com>
2021-11-30 22:18:34 -0700

    1p2 python solution

--- 01/puzzle01.py
+++ 01/puzzle01.py
@@ -10,3 +10,14 @@ for depth in measurements[1:]:
     last_depth = depth
 
 print(f"(p1 answer) increases = {increases}") #answer: 1548
+
+
+increases = 0
+last_window = measurements[0:3]
+for i in range(1, len(measurements)-2):
+    window = measurements[i:i+3]
+    if sum(window) > sum(last_window):
+        increases += 1
+    last_window = window
+
+print(f"(p2 answer) increases = {increases}") #answer: 1589
