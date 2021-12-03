df3065c8ddd288e8af13b76a948d576980a25d4a
Noah Yetter <nyetter@gmail.com>
2021-12-02 20:14:12 -0700

    python solution to 2p2

--- 02/puzzle02.py
+++ 02/puzzle02.py
@@ -20,3 +20,25 @@ for inst in instructions:
 print(f"position = {position}, depth = {depth}") # 2003, 980
 p1_answer = position * depth
 print(f"(p1 answer) position * depth = {p1_answer}") # 1962940
+
+
+position = 0
+depth = 0
+aim = 0
+for inst in instructions:
+    direction, value_str = inst.split(" ")
+    value = int(value_str)
+
+    if direction == "down":
+        aim += value
+    elif direction == "up":
+        aim -= value
+    elif direction == "forward":
+        position += value
+        depth += value * aim
+    else:
+        raise ValueError(f"unexpected direction: {direction}")
+
+print(f"position = {position}, depth = {depth}") # 2003, 905474
+p2_answer = position * depth
+print(f"(p2 answer) position * depth = {p2_answer}") # 1813664422
