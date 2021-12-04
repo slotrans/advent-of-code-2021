ea89ecf008f41b88da832694bf9ec0c44f1d60db
Noah Yetter <nyetter@gmail.com>
2021-12-03 19:10:02 -0700

    python solution to 3p2

--- other/03/puzzle03.py
+++ other/03/puzzle03.py
@@ -40,3 +40,29 @@ if __name__ == "__main__":
     print(f"epsilon rate = {epsilon_rate}") # 3896
     power_consumption = gamma_rate * epsilon_rate
     print(f"(p1 answer) power consumption = {power_consumption}") # 775304
+
+    ### part 2 ###
+
+    bit_string_length = len(bit_strings[0])
+    
+    temp_bit_strings = bit_strings.copy()
+    for i in range(0, bit_string_length):
+        most_common_bits = get_most_common_bits(temp_bit_strings)
+        temp_bit_strings = list(filter(lambda x: x[i] == most_common_bits[i], temp_bit_strings))
+        if len(temp_bit_strings) == 1:
+            break
+    oxygen_generator_rating = int("".join(temp_bit_strings[0]), 2)
+
+    temp_bit_strings = bit_strings.copy()
+    for i in range(0, bit_string_length):
+        least_common_bits = invert_bit_list(get_most_common_bits(temp_bit_strings))
+        temp_bit_strings = list(filter(lambda x: x[i] == least_common_bits[i], temp_bit_strings))
+        if len(temp_bit_strings) == 1:
+            break
+    co2_scrubber_rating = int("".join(temp_bit_strings[0]), 2)
+
+    print(f"oxygen generator rating = {oxygen_generator_rating}") # 509
+    print(f"co2 scrubber rating = {co2_scrubber_rating}") # 2693
+    life_support_rating = oxygen_generator_rating * co2_scrubber_rating
+    print(f"(p2 answer) life support rating = {life_support_rating}") # 1370737
+
