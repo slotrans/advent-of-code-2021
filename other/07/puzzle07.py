ebe081a732d30fec40e924c9365680f2c07d2a0b
Noah Yetter <nyetter@gmail.com>
2021-12-07 08:41:22 -0700

    (slow) python solution to 7p2

--- other/07/puzzle07.py
+++ other/07/puzzle07.py
@@ -1,5 +1,5 @@
 
-def minimize_fuel(positions):
+def minimize_fuel_p1(positions):
     min_position = min(positions)
     max_position = max(positions)
     #print(f"searching from {min_position} to {max_position}")
@@ -15,21 +15,57 @@ def minimize_fuel(positions):
     return best_position, best_total_fuel
 
 
+def fuel_usage_p2(x, y):
+    difference = abs(x - y)
+    return sum([i for i in range(0, difference+1)])
+
+
+def minimize_fuel_p2(positions):
+    min_position = min(positions)
+    max_position = max(positions)
+    #print(f"searching from {min_position} to {max_position}")
+    best_total_fuel = 2**62
+    best_position = None
+    for possible_position in range(min_position, max_position+1):
+        total_fuel = sum([fuel_usage_p2(x, possible_position) for x in positions])
+        #print(f"total fuel to move to position {possible_position}: {total_fuel}")
+        if total_fuel < best_total_fuel:
+            best_total_fuel = total_fuel
+            best_position = possible_position
+
+    return best_position, best_total_fuel
+
+
 SAMPLE_INPUT = "16,1,2,0,4,2,7,1,2,14"
 
 def sample_part1():
     print("SAMPLE Part 1")
     positions = [int(x) for x in SAMPLE_INPUT.split(",")]
-    best_position, total_fuel = minimize_fuel(positions)
+    best_position, total_fuel = minimize_fuel_p1(positions)
     print(f"best position = {best_position}, total_fuel = {total_fuel}")
     assert best_position == 2
     assert total_fuel == 37
 
 
+def sample_part2():
+    print("SAMPLE Part 1")
+    positions = [int(x) for x in SAMPLE_INPUT.split(",")]
+    best_position, total_fuel = minimize_fuel_p2(positions)
+    print(f"best position = {best_position}, total_fuel = {total_fuel}")
+    assert best_position == 5
+    assert total_fuel == 168
+
+
 def part1(positions):
     print("Part 1")
-    best_position, total_fuel = minimize_fuel(positions)
+    best_position, total_fuel = minimize_fuel_p1(positions)
     print(f"best position = {best_position}, (p1 answer) total_fuel = {total_fuel}") # 
+
+
+def part2(positions):
+    print("Part 2")
+    best_position, total_fuel = minimize_fuel_p2(positions)
+    print(f"best position = {best_position}, (p2 answer) total_fuel = {total_fuel}") # 
 
 
 if __name__ == "__main__":
@@ -38,3 +74,6 @@ if __name__ == "__main__":
 
     sample_part1()
     part1(initial_positions)
+
+    sample_part2()
+    part2(initial_positions)
