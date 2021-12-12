53337c253dc5cc54b548dcbc6fca7de7166358ab
Noah Yetter <nyetter@gmail.com>
2021-12-12 14:45:45 -0700

    kotlin solution to 12p2 required some refactor of the p1 code also redid most of the unit tests asserts because it turns out the argument order is (expected, actual) so the reports are misleading if you do (actual, expected) like a sane person

--- aoc2021kotlin/src/main/kotlin/Puzzle12.kt
+++ aoc2021kotlin/src/main/kotlin/Puzzle12.kt
@@ -1,9 +1,7 @@
 package net.blergh.advent2021
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import java.io.File
 import java.lang.Exception
 
@@ -26,24 +24,42 @@ object Puzzle12 {
 
     class Path {
         val nodes = mutableListOf<Node>()
+        val maxSmallVisits: Int
 
-        constructor(firstNode: Node) {
+        constructor(firstNode: Node, _maxSmallVisits: Int) {
             nodes.add(firstNode)
+            maxSmallVisits = _maxSmallVisits
         }
 
         constructor(otherPath: Path) {
             nodes.addAll(otherPath.nodes)
+            maxSmallVisits = otherPath.maxSmallVisits
         }
 
         // to ease testing
-        constructor(pathString: String) {
+        constructor(pathString: String, _maxSmallVisits: Int) {
             for(nodeName in pathString.split(",")) {
                 nodes.add(Node(nodeName))
             }
+            maxSmallVisits = _maxSmallVisits
         }
 
         fun isValidNextNode(n: Node): Boolean {
-            return !(n.size == CaveSize.SMALL && nodes.contains(n))
+            //return !(n.size == CaveSize.SMALL && nodes.contains(n))
+            if(n.size != CaveSize.SMALL) return true
+
+            if(nodes.contains(n) && (n == Node("start") || n == Node("end"))) return false
+
+            if(!nodes.contains(n)) return true
+
+            val mostVisitsToAnySmallCave = nodes.filter { it.size == CaveSize.SMALL }
+                .groupingBy { it }
+                .eachCount()
+                .values
+                .maxOf { it }
+            if(mostVisitsToAnySmallCave < maxSmallVisits) return true
+
+            return false
         }
 
         fun append(n: Node) {
@@ -81,12 +97,12 @@ object Puzzle12 {
             return adjacency.getOrDefault(refNode, setOf())
         }
 
-        fun startToEndPaths(): Set<Path> {
+        fun startToEndPaths(maxSmallVisits: Int): Set<Path> {
             // assumes every graph has at least one path from "start" to "end"
             if(paths.isEmpty()) {
                 val startNode = Node("start")
                 for(n in getAdjacentNodes(startNode)) {
-                    tracePath(Path(startNode), n)
+                    tracePath(Path(startNode, maxSmallVisits), n)
                 }
             }
 
@@ -124,9 +140,14 @@ object Puzzle12 {
         val input12 = File("${Main.aocRoot}/other/12/input12").readText().trim()
 
         println("Part 1")
-        val graph = Graph.fromInputString(input12)
-        val paths = graph.startToEndPaths()
-        println("(p1 answer) count of start->end paths: ${paths.size}")
+        val graph1 = Graph.fromInputString(input12)
+        val paths1 = graph1.startToEndPaths(1)
+        println("(p1 answer) count of start->end paths: ${paths1.size}") // 5178
+
+        println("Part 2")
+        val graph2 = Graph.fromInputString(input12)
+        val paths2 = graph2.startToEndPaths(2)
+        println("(p2 answer) count of start->end paths: ${paths2.size}") // 130094
     }
 
     /******************************************************************************************************************/
@@ -180,97 +201,157 @@ object Puzzle12 {
         fun `nodes from line`() {
             val inputLine = "A-b"
             val (nodeA, nodeB) = Node.pairFromInputLine(inputLine)
-            assertEquals(nodeA, Node("A"))
-            assertEquals(nodeA.size, CaveSize.BIG)
-            assertEquals(nodeB, Node("b"))
-            assertEquals(nodeB.size, CaveSize.SMALL)
+            assertEquals(Node("A"), nodeA)
+            assertEquals(CaveSize.BIG, nodeA.size)
+            assertEquals(Node("b"), nodeB)
+            assertEquals(CaveSize.SMALL, nodeB.size)
         }
 
         @Test
         fun `construct graph from sample input `() {
             val graph = Graph.fromInputString(SAMPLE_INPUT_1)
-            assertTrue(graph.getAdjacentNodes(Node("start")) == setOf(
+            assertEquals(setOf(
                 Node("A"),
                 Node("b"),
-            ))
-            assertTrue(graph.getAdjacentNodes(Node("A")) == setOf(
+            ), graph.getAdjacentNodes(Node("start")))
+            assertEquals(setOf(
                 Node("start"),
                 Node("b"),
                 Node("c"),
                 Node("end"),
-            ))
-            assertTrue(graph.getAdjacentNodes(Node("b")) == setOf(
+            ), graph.getAdjacentNodes(Node("A")))
+            assertEquals(setOf(
                 Node("start"),
                 Node("A"),
                 Node("d"),
                 Node("end"),
-            ))
-            assertTrue(graph.getAdjacentNodes(Node("c")) == setOf(
+            ), graph.getAdjacentNodes(Node("b")))
+            assertEquals(setOf(
                 Node("A"),
-            ))
-            assertTrue(graph.getAdjacentNodes(Node("d")) == setOf(
+            ), graph.getAdjacentNodes(Node("c")))
+            assertEquals(setOf(
                 Node("b"),
-            ))
-            assertTrue(graph.getAdjacentNodes(Node("end")) == setOf(
+            ), graph.getAdjacentNodes(Node("d")))
+            assertEquals(setOf(
                 Node("A"),
                 Node("b"),
-            ))
+            ), graph.getAdjacentNodes(Node("end")))
         }
 
         @Test
-        fun `paths in sample 1`() {
+        fun `paths in sample 1 with part1 rules`() {
             val graph = Graph.fromInputString(SAMPLE_INPUT_1)
-            val paths = graph.startToEndPaths()
+            val paths = graph.startToEndPaths(1)
             val expectedPaths = setOf(
-                Path("start,A,b,A,c,A,end"),
-                Path("start,A,b,A,end"),
-                Path("start,A,b,end"),
-                Path("start,A,c,A,b,A,end"),
-                Path("start,A,c,A,b,end"),
-                Path("start,A,c,A,end"),
-                Path("start,A,end"),
-                Path("start,b,A,c,A,end"),
-                Path("start,b,A,end"),
-                Path("start,b,end"),
+                Path("start,A,b,A,c,A,end", 1),
+                Path("start,A,b,A,end", 1),
+                Path("start,A,b,end", 1),
+                Path("start,A,c,A,b,A,end", 1),
+                Path("start,A,c,A,b,end", 1),
+                Path("start,A,c,A,end", 1),
+                Path("start,A,end", 1),
+                Path("start,b,A,c,A,end", 1),
+                Path("start,b,A,end", 1),
+                Path("start,b,end", 1),
             )
-            assertEquals(paths.size, expectedPaths.size) // 10
-            assertEquals(paths, expectedPaths)
+            assertEquals(expectedPaths.size, paths.size) // 10
+            assertEquals(expectedPaths, paths)
         }
 
         @Test
-        fun `paths in sample 2`() {
+        fun `paths in sample 2 with part1 rules`() {
             val graph = Graph.fromInputString(SAMPLE_INPUT_2)
-            val paths = graph.startToEndPaths()
+            val paths = graph.startToEndPaths(1)
             val expectedPaths = setOf(
-                Path("start,HN,dc,HN,end"),
-                Path("start,HN,dc,HN,kj,HN,end"),
-                Path("start,HN,dc,end"),
-                Path("start,HN,dc,kj,HN,end"),
-                Path("start,HN,end"),
-                Path("start,HN,kj,HN,dc,HN,end"),
-                Path("start,HN,kj,HN,dc,end"),
-                Path("start,HN,kj,HN,end"),
-                Path("start,HN,kj,dc,HN,end"),
-                Path("start,HN,kj,dc,end"),
-                Path("start,dc,HN,end"),
-                Path("start,dc,HN,kj,HN,end"),
-                Path("start,dc,end"),
-                Path("start,dc,kj,HN,end"),
-                Path("start,kj,HN,dc,HN,end"),
-                Path("start,kj,HN,dc,end"),
-                Path("start,kj,HN,end"),
-                Path("start,kj,dc,HN,end"),
-                Path("start,kj,dc,end"),
+                Path("start,HN,dc,HN,end", 1),
+                Path("start,HN,dc,HN,kj,HN,end", 1),
+                Path("start,HN,dc,end", 1),
+                Path("start,HN,dc,kj,HN,end", 1),
+                Path("start,HN,end", 1),
+                Path("start,HN,kj,HN,dc,HN,end", 1),
+                Path("start,HN,kj,HN,dc,end", 1),
+                Path("start,HN,kj,HN,end", 1),
+                Path("start,HN,kj,dc,HN,end", 1),
+                Path("start,HN,kj,dc,end", 1),
+                Path("start,dc,HN,end", 1),
+                Path("start,dc,HN,kj,HN,end", 1),
+                Path("start,dc,end", 1),
+                Path("start,dc,kj,HN,end", 1),
+                Path("start,kj,HN,dc,HN,end", 1),
+                Path("start,kj,HN,dc,end", 1),
+                Path("start,kj,HN,end", 1),
+                Path("start,kj,dc,HN,end", 1),
+                Path("start,kj,dc,end", 1),
             )
-            assertEquals(paths.size, expectedPaths.size) // 19
-            assertEquals(paths, expectedPaths)
+            assertEquals(expectedPaths.size, paths.size) // 19
+            assertEquals(expectedPaths, paths)
         }
 
         @Test
-        fun `path count in sample 3`() {
+        fun `path count in sample 3 with part1 rules`() {
             val graph = Graph.fromInputString(SAMPLE_INPUT_3)
-            val paths = graph.startToEndPaths()
-            assertEquals(paths.size, 226)
+            val paths = graph.startToEndPaths(1)
+            assertEquals(226, paths.size)
+        }
+
+        @Test
+        fun `paths in sample 1 with part2 rules`() {
+            val graph = Graph.fromInputString(SAMPLE_INPUT_1)
+            val paths = graph.startToEndPaths(2)
+            val expectedPaths = setOf(
+                Path("start,A,b,A,b,A,c,A,end", 2),
+                Path("start,A,b,A,b,A,end", 2),
+                Path("start,A,b,A,b,end", 2),
+                Path("start,A,b,A,c,A,b,A,end", 2),
+                Path("start,A,b,A,c,A,b,end", 2),
+                Path("start,A,b,A,c,A,c,A,end", 2),
+                Path("start,A,b,A,c,A,end", 2),
+                Path("start,A,b,A,end", 2),
+                Path("start,A,b,d,b,A,c,A,end", 2),
+                Path("start,A,b,d,b,A,end", 2),
+                Path("start,A,b,d,b,end", 2),
+                Path("start,A,b,end", 2),
+                Path("start,A,c,A,b,A,b,A,end", 2),
+                Path("start,A,c,A,b,A,b,end", 2),
+                Path("start,A,c,A,b,A,c,A,end", 2),
+                Path("start,A,c,A,b,A,end", 2),
+                Path("start,A,c,A,b,d,b,A,end", 2),
+                Path("start,A,c,A,b,d,b,end", 2),
+                Path("start,A,c,A,b,end", 2),
+                Path("start,A,c,A,c,A,b,A,end", 2),
+                Path("start,A,c,A,c,A,b,end", 2),
+                Path("start,A,c,A,c,A,end", 2),
+                Path("start,A,c,A,end", 2),
+                Path("start,A,end", 2),
+                Path("start,b,A,b,A,c,A,end", 2),
+                Path("start,b,A,b,A,end", 2),
+                Path("start,b,A,b,end", 2),
+                Path("start,b,A,c,A,b,A,end", 2),
+                Path("start,b,A,c,A,b,end", 2),
+                Path("start,b,A,c,A,c,A,end", 2),
+                Path("start,b,A,c,A,end", 2),
+                Path("start,b,A,end", 2),
+                Path("start,b,d,b,A,c,A,end", 2),
+                Path("start,b,d,b,A,end", 2),
+                Path("start,b,d,b,end", 2),
+                Path("start,b,end", 2),
+            )
+            assertEquals(expectedPaths.size, paths.size) // 36
+            assertEquals(expectedPaths, paths)
+        }
+
+        @Test
+        fun `path count in sample 2 with part2 rules`() {
+            val graph = Graph.fromInputString(SAMPLE_INPUT_2)
+            val paths = graph.startToEndPaths(2)
+            assertEquals(103, paths.size)
+        }
+
+        @Test
+        fun `path count in sample 3 with part2 rules`() {
+            val graph = Graph.fromInputString(SAMPLE_INPUT_3)
+            val paths = graph.startToEndPaths(2)
+            assertEquals(3509, paths.size)
         }
     }
 }
