54c027dd0d88b95cb1347ece38da92deb03a6317
Noah Yetter <nyetter@gmail.com>
2021-12-17 13:55:42 -0700

    kotlin solution to 16p2

--- aoc2021kotlin/src/main/kotlin/Puzzle16.kt
+++ aoc2021kotlin/src/main/kotlin/Puzzle16.kt
@@ -12,6 +12,7 @@ const val COUNT_TYPE = 1
 
 abstract class Packet(version: Int, typeID: Int) {
     abstract fun versionSum(): Int
+    abstract fun evaluate(): Long
 }
 
 data class LiteralPacket(
@@ -21,6 +22,10 @@ data class LiteralPacket(
     override
     fun versionSum(): Int {
         return version
+    }
+
+    override fun evaluate(): Long {
+        return literalValue
     }
 }
 
@@ -32,11 +37,70 @@ data class OperatorPacket(
 ): Packet(version, typeID) {
     init {
         assert(typeID != LITERAL_TYPE)
+        assert(typeID in (0..7))
     }
 
     override
     fun versionSum(): Int {
         return version + subPackets.sumOf { it.versionSum() }
+    }
+
+    override
+    fun evaluate(): Long {
+        return when(typeID) {
+            0 -> _sum()
+            1 -> _product()
+            2 -> _min()
+            3 -> _max()
+            5 -> _greaterThan()
+            6 -> _lessThan()
+            7 -> _equalTo()
+            else -> throw java.lang.Exception("unreachable")
+        }
+    }
+
+    private fun _sum(): Long {
+        return subPackets.sumOf { it.evaluate() }
+    }
+
+    private fun _product(): Long {
+        var product: Long = 1
+        for(p in subPackets) {
+            product *= p.evaluate()
+        }
+        return product
+    }
+
+    private fun _min(): Long {
+        return subPackets.minOf { it.evaluate() }
+    }
+
+    private fun _max(): Long {
+        return subPackets.maxOf { it.evaluate() }
+    }
+
+    private fun _greaterThan(): Long {
+        assert(subPackets.size == 2)
+        return when(subPackets[0].evaluate() > subPackets[1].evaluate()) {
+            true -> 1
+            false -> 0
+        }
+    }
+
+    private fun _lessThan(): Long {
+        assert(subPackets.size == 2)
+        return when(subPackets[0].evaluate() < subPackets[1].evaluate()) {
+            true -> 1
+            false -> 0
+        }
+    }
+
+    private fun _equalTo(): Long {
+        assert(subPackets.size == 2)
+        return when(subPackets[0].evaluate() == subPackets[1].evaluate()) {
+            true -> 1
+            false -> 0
+        }
     }
 }
 
@@ -47,7 +111,12 @@ object Puzzle16 {
 
         println("Part 1")
         val (bitsConsumed, packets) = parseBits(inputBits)
-        println("sum of packet versions = ${packets.first().versionSum()}") //
+        val thePacket = packets.first()
+        println("sum of packet versions = ${thePacket.versionSum()}") // 1002
+
+        println("Part 2")
+        val result = thePacket.evaluate()
+        println("result of evaluating packet = $result") // 1673210814091
     }
 
     val HEX_TO_BIN = mapOf(
@@ -167,8 +236,14 @@ object Puzzle16 {
         @Test
         fun `hex to binary`() {
             assertEquals("110100101111111000101000".toCharArray().toList(), inputHexToBinary("D2FE28"))
-            assertEquals("00111000000000000110111101000101001010010001001000000000".toCharArray().toList(), inputHexToBinary("38006F45291200"))
-            assertEquals("11101110000000001101010000001100100000100011000001100000".toCharArray().toList(), inputHexToBinary("EE00D40C823060"))
+            assertEquals(
+                "00111000000000000110111101000101001010010001001000000000".toCharArray().toList(),
+                inputHexToBinary("38006F45291200")
+            )
+            assertEquals(
+                "11101110000000001101010000001100100000100011000001100000".toCharArray().toList(),
+                inputHexToBinary("EE00D40C823060")
+            )
         }
 
         @Test
@@ -184,10 +259,12 @@ object Puzzle16 {
         fun `operator packet first example`() {
             //val inputBits = "0011100000000000011011110100010100101001000100100".toCharArray().toList() // trailing seven zeros omitted
             val inputBits = inputHexToBinary("38006F45291200")
-            val expected = OperatorPacket(1, 6, listOf(
-                LiteralPacket(6, 10),
-                LiteralPacket(2, 20),
-            ))
+            val expected = OperatorPacket(
+                1, 6, listOf(
+                    LiteralPacket(6, 10),
+                    LiteralPacket(2, 20),
+                )
+            )
             val (bitsConsumed, packets) = parseBits(inputBits)
             assertEquals(expected, packets.first())
         }
@@ -196,11 +273,13 @@ object Puzzle16 {
         fun `operator packet second example`() {
             //val inputBits = "111011100000000011010100000011001000001000110000011".toCharArray().toList() // trailing five zeros omitted
             val inputBits = inputHexToBinary("EE00D40C823060")
-            val expected = OperatorPacket(7, 3, listOf(
-                LiteralPacket(2, 1),
-                LiteralPacket(4, 2),
-                LiteralPacket(1, 3),
-            ))
+            val expected = OperatorPacket(
+                7, 3, listOf(
+                    LiteralPacket(2, 1),
+                    LiteralPacket(4, 2),
+                    LiteralPacket(1, 3),
+                )
+            )
             val (bitsConsumed, packets) = parseBits(inputBits)
             assertEquals(expected, packets.first())
         }
@@ -212,13 +291,19 @@ object Puzzle16 {
             // an operator packet (version 5) which contains
             // a literal value (version 6); this packet has a version sum of 16.
             val inputBits = inputHexToBinary("8A004A801A8002F478")
-            val expected = OperatorPacket(4, 2, listOf(
-                OperatorPacket(1, 2, listOf(
-                    OperatorPacket(5, 2, listOf(
-                        LiteralPacket(6, 15)
-                    ))
-                ))
-            ))
+            val expected = OperatorPacket(
+                4, 2, listOf(
+                    OperatorPacket(
+                        1, 2, listOf(
+                            OperatorPacket(
+                                5, 2, listOf(
+                                    LiteralPacket(6, 15)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
             val (bitsConsumed, packets) = parseBits(inputBits)
             assertEquals(expected, packets.first())
             assertEquals(16, packets.first().versionSum())
@@ -230,16 +315,22 @@ object Puzzle16 {
             // two sub-packets; each sub-packet is an operator packet that contains
             // two literal values. This packet has a version sum of 12.
             val inputBits = inputHexToBinary("620080001611562C8802118E34")
-            val expected = OperatorPacket(3, 0, listOf(
-                OperatorPacket(0, 0, listOf(
-                    LiteralPacket(0, 10),
-                    LiteralPacket(5, 11),
-                )),
-                OperatorPacket(1, 0, listOf(
-                    LiteralPacket(0,12),
-                    LiteralPacket(3,13),
-                )),
-            ))
+            val expected = OperatorPacket(
+                3, 0, listOf(
+                    OperatorPacket(
+                        0, 0, listOf(
+                            LiteralPacket(0, 10),
+                            LiteralPacket(5, 11),
+                        )
+                    ),
+                    OperatorPacket(
+                        1, 0, listOf(
+                            LiteralPacket(0, 12),
+                            LiteralPacket(3, 13),
+                        )
+                    ),
+                )
+            )
             val (bitsConsumed, packets) = parseBits(inputBits)
             assertEquals(expected, packets.first())
             assertEquals(12, packets.first().versionSum())
@@ -254,7 +345,7 @@ object Puzzle16 {
             val (bitsConsumed, packets) = parseBits(inputBits)
             val outerPacket = packets.first()
             assertTrue(outerPacket is OperatorPacket)
-            if(outerPacket is OperatorPacket) {
+            if (outerPacket is OperatorPacket) {
                 assertTrue(outerPacket.subPackets[0] is OperatorPacket)
                 assertTrue(outerPacket.subPackets[1] is OperatorPacket)
             }
@@ -271,13 +362,13 @@ object Puzzle16 {
             val (bitsConsumed, packets) = parseBits(inputBits)
             val outerPacket = packets.first()
             assertTrue(outerPacket is OperatorPacket)
-            if(outerPacket is OperatorPacket) {
+            if (outerPacket is OperatorPacket) {
                 val innerPacket1 = outerPacket.subPackets[0]
                 assertTrue(innerPacket1 is OperatorPacket)
-                if(innerPacket1 is OperatorPacket) {
+                if (innerPacket1 is OperatorPacket) {
                     val innerPacket2 = innerPacket1.subPackets[0]
                     assertTrue(innerPacket2 is OperatorPacket)
-                    if(innerPacket2 is OperatorPacket) {
+                    if (innerPacket2 is OperatorPacket) {
                         assertTrue(innerPacket2.subPackets[0] is LiteralPacket)
                         assertTrue(innerPacket2.subPackets[1] is LiteralPacket)
                         assertTrue(innerPacket2.subPackets[2] is LiteralPacket)
@@ -287,6 +378,102 @@ object Puzzle16 {
                 }
             }
             assertEquals(31, outerPacket.versionSum())
+        }
+
+        @Test
+        fun `sum operator, p2 example 1`() {
+            //C200B40A82 finds the sum of 1 and 2, resulting in the value 3.
+            val inputBits = inputHexToBinary("C200B40A82")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(3, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `product operator, p2 example 2`() {
+            //04005AC33890 finds the product of 6 and 9, resulting in the value 54.
+            val inputBits = inputHexToBinary("04005AC33890")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(54, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `min operator, p2 example 3`() {
+            //880086C3E88112 finds the minimum of 7, 8, and 9, resulting in the value 7.
+            val inputBits = inputHexToBinary("880086C3E88112")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(7, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `max operator, p2 example 4`() {
+            //CE00C43D881120 finds the maximum of 7, 8, and 9, resulting in the value 9.
+            val inputBits = inputHexToBinary("CE00C43D881120")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(9, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `less-than operator, p2 example 5`() {
+            //D8005AC2A8F0 produces 1, because 5 is less than 15.
+            val inputBits = inputHexToBinary("D8005AC2A8F0")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(1, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `greater-than operator, p2 example 6`() {
+            //F600BC2D8F produces 0, because 5 is not greater than 15.
+            val inputBits = inputHexToBinary("F600BC2D8F")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(0, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `equal-to operator, p2 example 7`() {
+            //9C005AC2F8F0 produces 0, because 5 is not equal to 15.
+            val inputBits = inputHexToBinary("9C005AC2F8F0")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(0, outerPacket.evaluate())
+            }
+        }
+
+        @Test
+        fun `combination of operators, p2 example 8`() {
+            //9C0141080250320F1802104A08 produces 1, because 1 + 3 = 2 * 2.
+            val inputBits = inputHexToBinary("9C0141080250320F1802104A08")
+            val (bitsConsumed, packets) = parseBits(inputBits)
+            val outerPacket = packets.first()
+            assertTrue(outerPacket is OperatorPacket)
+            if(outerPacket is OperatorPacket) {
+                assertEquals(1, outerPacket.evaluate())
+            }
         }
     }
 }
